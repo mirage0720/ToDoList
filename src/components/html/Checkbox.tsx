c502f3c2e135b3e7cc0ed8c8e598f3a739425a91
mirage0720 <youdw1994@gmail.com>
2025-09-08 12:04:23 +0900

    Initial commit

---
+++ src/components/html/Checkbox.tsx
@@ -0,0 +1,16 @@
+type CheckboxProps = Omit<React.ComponentPropsWithoutRef<"input">, "type"> & {
+  type?: "checkbox";
+  parentClassName?: string;
+};
+
+export default function Checkbox(props: CheckboxProps) {
+  const { parentClassName, children, ...rest } = props;
+  return (
+    <>
+      <div className={parentClassName}>
+        <input {...rest} />
+        <label>{children}</label>
+      </div>
+    </>
+  );
+}
